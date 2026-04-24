99268ffb697894f871eb3c5fb59f9da4889fe49e
Lev1C1 <levi.cirino1006@gmail.com>
2026-04-23 21:57:21 -0300

    Adição das Querys ao SerieRepository

--- 3355-java-cinehub-com-jpa-main/src/main/java/br/com/cinehub/repository/SerieRepository.java
+++ 3355-java-cinehub-com-jpa-main/src/main/java/br/com/cinehub/repository/SerieRepository.java
@@ -1,9 +1,36 @@
 package br.com.cinehub.repository;
 
+import br.com.cinehub.model.Categoria;
+import br.com.cinehub.model.Episodio;
 import br.com.cinehub.model.Serie;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface SerieRepository extends JpaRepository<Serie, Long> {
-    Optional<Serie> findByTitulo(String titulo);
+    Optional<Serie> findByTituloContainingIgnoreCase(String nomeSerie);
+
+    List<Serie> findByAtoresContainingIgnoreCaseAndAvaliacaoGreaterThanEqual(String nomeAtor, Double avaliacao);
+
+    List<Serie> findTop5ByOrderByAvaliacaoDesc();
+
+    List<Serie> findByGenero(Categoria categoria);
+
+    //List<Serie> findByTotalTemporadasLessThanEqualAndAvaliacaoGreaterThanEqual(int totalTemporadas, Double avaliacao);
+
+    @Query("SELECT s FROM Serie s WHERE s.totalTemporadas <= :totalTemporadas AND s.avaliacao >= :avaliacao")
+    List<Serie> seriesPorTemporadaEAvaliacao(int totalTemporadas, Double avaliacao);
+
+    @Query("SELECT e FROM Serie s JOIN s.episodios e WHERE e.titulo ILIKE %:trechoEpisodio%")
+    List<Episodio> episodiosPorTrecho(String trechoEpisodio);
+
+    @Query("SELECT e FROM Episodio e WHERE e.serie = :serie ORDER BY e.avaliacao DESC")
+    List<Episodio> topEpisodiosPorSerie(
+            @Param("serie") Serie serie,
+            Pageable pageable
+    );
 }
